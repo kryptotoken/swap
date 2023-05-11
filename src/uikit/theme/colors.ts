8b98ed1a573ccb06dbc5aa4e5c9f7f08fb5ea7a9
powman <paulophpweb@gmail.com>
2023-05-11 13:02:37 -0300

    Update

---
+++ src/uikit/theme/colors.ts
@@ -0,0 +1,49 @@
+import { Colors } from "./types";
+
+export const baseColors = {
+  failure: "#ED4B9E",
+  primary: "#E12A26",
+  primaryBright: "#E12A26",
+  primaryDark: "##E12A26",
+  secondary: "#0e0d6b",
+  success: "#E6D226",
+  warning: "#FFB237",
+};
+
+export const brandColors = {
+  binance: "#F0B90B",
+};
+
+export const lightColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  background: "#FAF9FA",
+  backgroundDisabled: "#E9EAEB",
+  contrast: "#191326",
+  invertedContrast: "#FFFFFF",
+  input: "#eeeaf4",
+  tertiary: "#EFF4F5",
+  text: "#0d0b85",
+  textDisabled: "#BDC2C4",
+  textSubtle: "#E6D226",
+  borderColor: "#E9EAEB",
+  card: "#FFFFFF",
+};
+
+export const darkColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  secondary: "#5fafee",
+  background: "#343135",
+  backgroundDisabled: "#3c3742",
+  contrast: "#FFFFFF",
+  invertedContrast: "#191326",
+  input: "#483f5a",
+  primaryDark: "#0098A1",
+  tertiary: "#353547",
+  text: "#EAE2FC",
+  textDisabled: "#666171",
+  textSubtle: "#c9c4d4",
+  borderColor: "#524B63",
+  card: "#27262c",
+};
