5a24f77feca7b29525d06ad06862c8afe5a38ea1
powman <paulophpweb@gmail.com>
2023-05-11 18:26:51 -0300

    fsd

--- src/hooks/useGetPriceData.ts
+++ src/hooks/useGetPriceData.ts
@@ -1,18 +1,10 @@
 import { useEffect, useState } from 'react'
 import { usePairContract } from './useContract'
 
-type ApiResponse = {
-  prices: {
-    [key: string]: string
-  }
-  update_at: string
-}
-
 /**
  * Due to Cors the api was forked and a proxy was created
  * @see https://github.com/pancakeswap/gatsby-pancake-api/commit/e811b67a43ccc41edd4a0fa1ee704b2f510aa0ba
  */
-const api = 'https://api.pancakeswap.com/api/v1/price'
 
 const useGetPriceData = () => {
 
